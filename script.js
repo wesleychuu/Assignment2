992d0939a0c83f6eacf8ab83d7986280dbec0583
wesleychuu <wesleychu@telus.net>
2021-11-16 15:40:40 -0800

    created index functions

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+function cleanUpIndex() {
+    elements = document.querySelectorAll('.contact')
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].remove()
+    }
+}
+
+function createSingleIndex(name) {
+    
+}
+
+let contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+]
+
+
+
+function renderIndex(contact) {
+
+    let main = document.querySelectorAll('.main');
+       
+    for (let i = 0; i < contact.length; i++) {
+        let createA = document.createElement('a');
+        let createDiv = document.createElement('div');
+        let createP = document.createElement('p')
+
+        createP.append(contact[i].name);
+        createDiv.appendChild(createP);
+        createDiv.classList.add("contact");
+
+        createA.appendChild(createDiv);
+        createA.href = "page3.html";
+
+        main[0].appendChild(createA);
+    }
+}
+
